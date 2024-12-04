5582763daa23f947dc1900d32ed7d1d834b16c73
gbaleony499 <gbafleleon@gmail.com>
2024-12-04 13:41:52 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 // Test basique pour vérifier le déploiement sur Vercel
 echo "<h1>Test Vercel PHP</h1>";
 echo "<p>Félicitations ! Votre application PHP fonctionne sur Vercel.</p>";
+var_dump($_SERVER);
 
 // Informations supplémentaires
 echo "<h2>Informations de Debug</h2>";
